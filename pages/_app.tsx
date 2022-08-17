98cdabbf6f6ffc03bd5b1ea731dd7dda4bae2924
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2022-08-17 20:02:50 +0530

    complete tutorial

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,66 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import {
+  darkTheme,
+  getDefaultWallets,
+  RainbowKitProvider,
+} from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  chain,
+  configureChains,
+  createClient,
+  useSigner,
+  WagmiConfig,
+} from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { ChainId, ThirdwebSDKProvider } from "@thirdweb-dev/react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const { chains, provider } = configureChains(
+  [chain.polygonMumbai],
+  [
+    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API }),
+    publicProvider(),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+function ThirdwebProvider({ wagmiClient, children }: any) {
+  const { data: signer } = useSigner();
+
+  return (
+    <ThirdwebSDKProvider
+      desiredChainId={ChainId.Mumbai}
+      signer={signer as any}
+      provider={wagmiClient.provider}
+      queryClient={wagmiClient.queryClient as any}
+    >
+      {children}
+    </ThirdwebSDKProvider>
+  );
 }
 
-export default MyApp
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains} theme={darkTheme()}>
+        <ThirdwebProvider wagmiClient={wagmiClient}>
+          <Component {...pageProps} />
+        </ThirdwebProvider>
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
+
+export default MyApp;
